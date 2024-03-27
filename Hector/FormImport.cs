f853e04bbf049f076b5d20c385949652c4211c33
Genewolf <79755741+Genewolf@users.noreply.github.com>
2024-03-27 16:02:32 +0100

    add: requetes sql issues de la lecture du csv
    problème "database is locked" ligne 83 de formimport.cs

--- Hector/FormImport.cs
+++ Hector/FormImport.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Data.SQLite;
 
 namespace Hector
 {
@@ -28,37 +29,6 @@ namespace Hector
         {
             labelFileName.Text = openFileDialog1.SafeFileName;
             txtDirectoryPath.Text = openFileDialog1.FileName;
-            /*using (StreamReader Sr = File.OpenText(openFileDialog1.FileName))
-            {
-                string Ligne = "";
-                textBox1.Text = Ligne;
-                while ((Ligne = Sr.ReadLine()) != null)
-                {
-                    //On rajoute chaque ligne du fichier dans la Textbox
-                    textBox1.Text += Ligne;
-                    textBox1.AppendText(Environment.NewLine);
-                }
-                if (etoile)
-                {
-                    etoile = false;
-                }
-                Text = OGformName + "[" + openFileDialog1.SafeFileName + "]";
-
-                var watcher = new FileSystemWatcher(openFileDialog1.FileName);
-                watcher.NotifyFilter = NotifyFilters.Attributes
-                                     | NotifyFilters.CreationTime
-                                     | NotifyFilters.DirectoryName
-                                     | NotifyFilters.FileName
-                                     | NotifyFilters.LastAccess
-                                     | NotifyFilters.LastWrite
-                                     | NotifyFilters.Security
-                                     | NotifyFilters.Size;
-
-                watcher.Changed += OnChanged;
-                watcher.Filter = "*.txt";
-                watcher.IncludeSubdirectories = true;
-                watcher.EnableRaisingEvents = true;
-            }*/
         }
 
         //Fonctions qui font avancer les barres de progression à chaque avancée
@@ -81,13 +51,14 @@ namespace Hector
             }
         }
 
-        private void btnEcrase_Click(object sender, EventArgs e)
+        private void AjoutCSV_dans_SQLite()
         {
             progressBar1.Maximum = TotalLines(openFileDialog1.FileName);    //on setup le max de la barre de progression pour voir l'avancée par ligne
             progressBar1.Value = 0;                                         //on met la barre de progression à 0
             using (var Sr = new StreamReader(openFileDialog1.FileName))
             {
                 UpdateProgress();   //on prend en compte la lecture de la ligne de vérification dans la progression
+                //on vérifie si le format du fichier CSV est bon
                 if (Sr.ReadLine() != "Description;Ref;Marque;Famille;Sous-Famille;Prix H.T.") MessageBox.Show("Ce fichier csv n'est pas dans un bon format");
                 else
                 {
@@ -96,23 +67,110 @@ namespace Hector
                         string line = Sr.ReadLine();
                         string[] values = line.Split(';');
 
-                        //si la ref de l'article values[1] existe déjà on renvoie une erreur (ou on la compte juste pas en la stockant dans une liste d'echec)
-                        //si la marque values[2] existe pas on la crée et on récupère son ID
-                        //sinon on récupère l'ID de la marque existante
-                        //si la famille values[3] existe pas on la crée
-                        //si la sous-famille values[4] existe pas on la crée
-                        //sinon on récupère l'ID de la sous-famille existante
-                        //on ajoute l'article avec sa ref values[1], son nom values[0], sa sous-famille values[4], sa marque values[2], son prix values [5], et sa quantité à 0
+                        string DBPath = Path.Combine(Application.StartupPath, "Hector2.SQLite");
+                        string ConncetionString = @"Data Source=" + DBPath + ";";
 
+                        //On ouvre un SQLiteConnection pour faire des tests, puis ajouter les valeurs du .csv dans le .SQLite
+                        using (SQLiteConnection Database = new SQLiteConnection(ConncetionString))
+                        {
+                            Database.Open();
+                            SQLiteCommand selectCommand, insertCommand, replaceCommand;
+                            SQLiteDataReader query;
+
+                            string IDmarque = "";
+                            selectCommand = new SQLiteCommand("SELECT RefMarque, Nom FROM Marques WHERE Nom = '"+ values[2] + "'");
+                            selectCommand.Connection = Database;
+                            query = selectCommand.ExecuteReader();
+                            //CAS1.1: si la marque values[2] existe on récupère son ID
+                            if (query.Read())
+                            {
+                                IDmarque = query.GetInt32(0).ToString();
+                            }
+                            //CAS 1.2: sinon on la crée et on récupère son ID ensuite
+                            else
+                            {
+                                //Création de la marque
+                                insertCommand = new SQLiteCommand("INSERT INTO Marques (Nom) VALUES ('" + values[2] + "')");
+                                insertCommand.Connection = Database;
+                                insertCommand.ExecuteNonQuery();
+
+                                //Récupération de son ID
+                                selectCommand = new SQLiteCommand("SELECT RefMarque FROM Marques WHERE Nom = '" + values[2] + "'");
+                                selectCommand.Connection = Database;
+                                query = selectCommand.ExecuteReader();
+                                query.Read();
+                                IDmarque = query.GetInt32(0).ToString();
+                            }
+
+                            selectCommand = new SQLiteCommand("SELECT RefFamille, Nom FROM Familles WHERE Nom = '" + values[3] + "'");
+                            selectCommand.Connection = Database;
+                            query = selectCommand.ExecuteReader();
+                            //CAS 2: si la famille values[3] n'existe pas on la crée
+                            if (!query.Read())
+                            {
+                                insertCommand = new SQLiteCommand("INSERT INTO Famille (Nom) VALUES ('" + values[3] + "')");
+                                insertCommand.Connection = Database;
+                                insertCommand.ExecuteNonQuery();
+                            }
+
+                            string IDsousfamille = "";
+                            selectCommand = new SQLiteCommand("SELECT RefSousFamille, RefFamille, Nom FROM SousFamilles WHERE Nom = '" + values[4] + "'");
+                            selectCommand.Connection = Database;
+                            query = selectCommand.ExecuteReader();
+                            //CAS 3.1: si la sous-famille values[4] existe on récupère son ID
+                            if (query.Read())
+                            {
+                                IDsousfamille = query.GetInt32(0).ToString();
+                            }
+                            //CAS 3.2: sinon on la crée et on récupère son ID ensuite
+                            else
+                            {
+                                //Récupération de l'ID de la famille
+                                selectCommand = new SQLiteCommand("SELECT RefFamille FROM Familles WHERE Nom = '" + values[3] + "'");
+                                selectCommand.Connection = Database;
+                                query = selectCommand.ExecuteReader();
+                                query.Read();
+                                string IDfamille = query.GetInt32(0).ToString();
+
+                                //Création de la sous-famille
+                                insertCommand = new SQLiteCommand("INSERT INTO SousFamilles (RefFamille, Nom) VALUES ('" + IDfamille + "', '" + values[4] + "')");
+                                insertCommand.Connection = Database;
+                                insertCommand.ExecuteNonQuery();
+
+                                //Récupération de son ID
+                                selectCommand = new SQLiteCommand("SELECT RefSousFamille FROM SousFamilles WHERE Nom = '" + values[4] + "'");
+                                selectCommand.Connection = Database;
+                                query = selectCommand.ExecuteReader();
+                                query.Read();
+                                IDsousfamille = query.GetInt32(0).ToString();
+                            }
+
+                            //on remplace la virgule du float du prix par un point pour etre dans le format des requetes SQL
+                            string prix = values[5].Replace(',', '.');
+                            //on ajoute l'article avec sa ref values[1], son nom values[0], ID de sa sous-famille values[4], ID de sa marque values[2], son prix values [5], et sa quantité à 0
+                            //on utilise REPLACE pour mettre à jour l'article si son ID existe déjà
+                            replaceCommand = new SQLiteCommand("INSERT OR REPLACE INTO Articles (RefArticle, Description, RefSousFamille, RefMarque, PrixHT, Quantite)"+
+                                " VALUES ('" + values[1] +"', '" + values[0] + "', '" + IDsousfamille + "', '" + IDmarque + "', " + prix +", 0)");
+                            replaceCommand.Connection = Database;
+                            replaceCommand.ExecuteNonQuery();
+
+                            Database.Close();
+                        }
                         UpdateProgress();
                     }
                 }
             }
         }
 
+        private void btnEcrase_Click(object sender, EventArgs e)
+        {
+            //supprimer la base de données actuelle
+            AjoutCSV_dans_SQLite();
+        }
+
         private void btnAjout_Click(object sender, EventArgs e)
         {
-
+            AjoutCSV_dans_SQLite();
         }
     }
 }
