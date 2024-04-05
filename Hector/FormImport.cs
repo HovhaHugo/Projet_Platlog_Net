26c6e27264897f3dce9833892ac2c5c819bedd0a
Genewolf <79755741+Genewolf@users.noreply.github.com>
2024-04-05 14:55:51 +0200

    Edit: FormImport.cs
    Amélioration des commentaire, et mise en commentaire des fonctions qui créent le problème de database locked --> l'importation n'ajoute que les articles, avec des ID de 1

--- Hector/FormImport.cs
+++ Hector/FormImport.cs
@@ -13,11 +13,13 @@ namespace Hector
             InitializeComponent();
         }
 
+        //Fonction qui ouvre le openFileDialog lorsque l'on appuie sur le bouton pour chercher un fichier csv
         private void btnOpenFileDialog_Click(object sender, EventArgs e)
         {
             openFileDialog1.ShowDialog();
         }
 
+        //Fonction qui actualise le label du nom du fichier et le path du fichier lorsque l'on en a sélectionné un via le openFileDialog
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
             labelFileName.Text = openFileDialog1.SafeFileName;
@@ -34,6 +36,7 @@ namespace Hector
             }
         }
 
+        //Fonction qui compte le nombre totale de lignes dans un fichier csv (pour la barre de progression)
         private int TotalLines(string filePath)
         {
             using (StreamReader r = new StreamReader(filePath))
@@ -44,7 +47,7 @@ namespace Hector
             }
         }
 
-        //VERIFICATION DE L'EXISTENCE DE LA MARQUE / CREATION DE LA MARQUE SINON
+        //Fonction pour vérifier l'existence d'une marque à partir de son nom, et la créer si ce n'est pas le cas
         private void CheckMarque(SQLiteConnection Database, string nomMarque)
         {
             Database.Open();
@@ -61,7 +64,7 @@ namespace Hector
             Database.Close();
         }
 
-        //VERIFICATION DE L'EXISTENCE DE LA FAMILLE / CREATION DE LA FAMILLE SINON
+        //Fonction pour vérifier l'existence d'une famille à partir de son nom, et la créer si ce n'est pas le cas
         private void CheckFamille(SQLiteConnection Database, string nomFamille)
         {
             Database.Open();
@@ -78,7 +81,7 @@ namespace Hector
             Database.Close();
         }
 
-        //VERIFICATION DE L'EXISTENCE DE LA SOUS-FAMILLE / CREATION DE LA SOUS-FAMILLE SINON
+        //Fonction pour vérifier l'existence d'une sous-famille à partir de son nom, et la créer si ce n'est pas le cas
         private void CheckSousFamille(SQLiteConnection Database, string nomFamille, string nomSousFamille)
         {
             Database.Open();
@@ -105,7 +108,9 @@ namespace Hector
             Database.Close();
         }
 
-            private void AjoutCSV_dans_SQLite()
+        //Fonction qui va lire le fichier csv obtenu à partir du openFileDialog, puis entrer un à un les articles,
+        //en prenant soin de crée les marques/familles/sous-familles au fur et à mesure qu'on en a besoin, si jamais ils n'existent pas
+        private void AjoutCSV_dans_SQLite()
         {
             progressBar.Maximum = TotalLines(openFileDialog1.FileName);    //on setup le max de la barre de progression pour voir l'avancée par ligne
             progressBar.Value = 0;                                         //on met la barre de progression à 0
@@ -115,39 +120,44 @@ namespace Hector
                 if (Sr.ReadLine() != "Description;Ref;Marque;Famille;Sous-Famille;Prix H.T.") MessageBox.Show("Ce fichier csv n'est pas dans un bon format (en-tête)");
                 else
                 {
-                    UpdateProgress();   //on prend en compte la lecture de la ligne de vérification dans la progression
+                    UpdateProgress();   //pour la barre de progression, on prend en compte la lecture de la ligne de vérification
                     while (!Sr.EndOfStream)
                     {
                         string line = Sr.ReadLine();
                         string[] values = line.Split(';');
 
-                        string DBPath = Path.Combine(Application.StartupPath, "Hector2.SQLite");  //le path vers la base SQLite que l'on remplit
+                        string DBPath = Path.Combine(Application.StartupPath, "Hector.SQLite");  //le path vers la base SQLite que l'on remplit
                         string ConnectionString = @"Data Source=" + DBPath + ";";
 
                         //On ouvre un SQLiteConnection pour faire des tests, puis ajouter les valeurs du .csv dans le .SQLite
                         using (SQLiteConnection Database = new SQLiteConnection(ConnectionString))
                         {
-                            //On récupère l'ID de la marque values[2]
+                            /*
+                            //On crée la marque values[2] si elle n'existe pas
                             CheckMarque(Database, values[2]);
 
                             //On crée la famille values[3] si elle n'existe pas
                             CheckFamille(Database, values[3]);
 
-                            //On récupère l'ID de la sous-famille values[4]
+                            //On crée la sous-famille values[4] si elle n'existe pas
                             CheckSousFamille(Database, values[3], values[4]);
-
+                            */
                             //CREATION DE L'ARTICLE
 
                             string prix = values[5].Replace(',', '.'); //on remplace la virgule du float du prix par un point pour etre dans le format des requetes SQL
 
                             //on ajoute l'article avec sa ref values[1], son nom values[0], ID de sa sous-famille values[4], ID de sa marque values[2], son prix values [5], et sa quantité à 0
                             //on utilise INSERT OR REPLACE pour mettre à jour l'article si son ID existe déjà
-                            Database.Open();
-                            string RequeteAjoutArticle = "INSERT OR REPLACE INTO Articles (RefArticle, Description, RefSousFamille, RefMarque, PrixHT, Quantite)" +
+
+                            /*string RequeteAjoutArticle = "INSERT OR REPLACE INTO Articles (RefArticle, Description, RefSousFamille, RefMarque, PrixHT, Quantite)" +
                                 " VALUES ('" + values[1] + "', '" + values[0] + "', " +
                                 "(SELECT RefSousFamille FROM SousFamilles WHERE Nom = '" + values[4] + "'), " +
-                                "(SELECT RefMarque FROM Marques WHERE Nom = '" + values[2] + "'), " + prix + ", 0)";
+                                "(SELECT RefMarque FROM Marques WHERE Nom = '" + values[2] + "'), " + prix + ", 0)";*/
 
+                            string RequeteAjoutArticle = "INSERT OR REPLACE INTO Articles (RefArticle, Description, RefSousFamille, RefMarque, PrixHT, Quantite)" +
+                                " VALUES ('" + values[1] + "', '" + values[0] + "', '1', '1', " + prix + ", 0)";
+
+                            Database.Open();
                             using (SQLiteCommand Command = new SQLiteCommand(RequeteAjoutArticle, Database))
                                 Command.ExecuteNonQuery();
                             Database.Close();
@@ -170,7 +180,11 @@ namespace Hector
 
         private void btnAjout_Click(object sender, EventArgs e)
         {
-            AjoutCSV_dans_SQLite();
+            if (openFileDialog1.CheckFileExists)
+            {
+                AjoutCSV_dans_SQLite();
+            }
+            else MessageBox.Show("Veuillez choisir un tableau csv via le bouton 'Search file'");
         }
     }
 }
